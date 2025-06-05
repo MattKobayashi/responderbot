b115c3713fdb0f9ac5f5f90c75ae1758f72a84ac
Matthew Kobayashi <matthew@kobayashi.au>
2025-06-05 11:03:53 +1000

    refactor: clean up regex handling and improve user/channel event matching

--- responderbot.py
+++ responderbot.py
@@ -4,6 +4,10 @@ import os
 # For regex stuffs
 import re
 
+# For logging stuffs
+# import sys
+# import logging
+
 # Use the package we installed
 from slack_bolt import App
 
@@ -14,22 +18,7 @@ app = App(
 )
 
 
-@app.message(re.compile("github.com"))
-def github_regex(message, say):
-    whodunit = message["user"]
-    bot_response = (
-        f"Uh oh, <@{whodunit}> owes a round of beers for linking to their :github:"
-    )
-    bot_username = "GitHub Watch"
-    bot_icon_emoji = ":github:"
-    say(
-        username=bot_username,
-        icon_emoji=bot_icon_emoji,
-        text=bot_response,
-    )
-
-
-@app.message(re.compile("(skeeve|sleeve)"))
+@app.message(re.compile("(skeeve)"))
 def skeeve_regex(message, say):
     whodunit = message["user"]
     bot_response = f"Uh oh, <@{whodunit}> owes a round of beers for mentioning :skeeve:"
@@ -42,11 +31,96 @@ def skeeve_regex(message, say):
     )
 
 
-@app.event({
-    "user": "UEVC4T56E"
-    })
-def jen_rolleyes(channel, ts, client):
-    client.reactions_add(ts=ts, channel=channel, name='face_with_rolling_eyes')
+def isuser(userid: str):
+    def isuserevent(event: dict) -> bool:
+        return event["user"] == userid
+    return isuserevent
+
+
+def ischannel(channelid: str):
+    def ischannelevent(event: dict) -> bool:
+        return event["channel"] == channelid
+    return ischannelevent
+
+
+# # Hobl: Hobl
+# @app.event(
+#     event="message",
+#     matchers=[
+#         isuser("UET7N6EEL"),
+#         ischannel("C029TAFEP7B")
+#     ]
+# )
+# def hobl(client, event):
+#     client.reactions_add(
+#         timestamp=event["ts"],
+#         channel=event["channel"],
+#         name='hobl'
+#     )
+# 
+# 
+# # Lochie: Surprise Trout 3
+# @app.event(
+#     event="message",
+#     matchers=[
+#         isuser("U01QF2XTXRR"),
+#         ischannel("C029TAFEP7B")
+#     ]
+# )
+# def lochie(client, event):
+#     client.reactions_add(
+#         timestamp=event["ts"],
+#         channel=event["channel"],
+#         name='surprise_trout_3'
+#     )
+# 
+# 
+# # Bordy: Westnet
+# @app.event(
+#     event="message",
+#     matchers=[
+#         isuser("UEV3QR71D"),
+#         ischannel("C029TAFEP7B")
+#     ]
+# )
+# def bordy(client, event):
+#     client.reactions_add(
+#         timestamp=event["ts"],
+#         channel=event["channel"],
+#         name='westnet'
+#     )
+# 
+# 
+# # Brendan: Tasmania
+# @app.event(
+#     event="message",
+#     matchers=[
+#         isuser("UGCTE8DNJ"),
+#         ischannel("C029TAFEP7B")
+#     ]
+# )
+# def brendan(client, event):
+#     client.reactions_add(
+#         timestamp=event["ts"],
+#         channel=event["channel"],
+#         name='tasmania'
+#     )
+
+
+# # MattK: Test
+# @app.event(
+#     event="message",
+#     matchers=[
+#         isuser("UEVC4T56E"),
+#         ischannel("C03L85NTEQP")
+#     ]
+# )
+# def mattk(client, event):
+#     client.reactions_add(
+#         timestamp=event["ts"],
+#         channel=event["channel"],
+#         name='face_with_rolling_eyes'
+#     )
 
 
 @app.event("message")
@@ -56,4 +130,5 @@ def handle_message_events(body, logger):
 
 # Start your app
 if __name__ == "__main__":
+    # logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     app.start(port=int(os.environ.get("PORT", 3000)))
